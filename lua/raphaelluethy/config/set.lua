3c78285377e78f5d0bdcfa7572f02a3f31e711cb
Raphael Lüthy <raphael@luethy.io>
2024-01-15 10:13:52 +0100

    fix: added undodir

--- lua/raphaelluethy/config/set.lua
+++ lua/raphaelluethy/config/set.lua
@@ -59,3 +59,7 @@ vim.g.backspace = 'indent,eol,start'
 vim.o.conceallevel = 0
 
 vim.o.scrolloff=8
+
+-- undo dir
+vim.opt.undodir = vim.fn.expand('~/.local/share/nvim/undo//')
+vim.o.undofile = true
