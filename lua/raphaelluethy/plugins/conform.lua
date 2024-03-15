fa7b05b41644735f7edacd517bc3965070b7166b
Raphael Lüthy <raphael.luethy@fhnw.ch>
2024-03-15 10:50:08 +0100

    fix: made nvim load faster

--- lua/raphaelluethy/plugins/conform.lua
+++ lua/raphaelluethy/plugins/conform.lua
@@ -1,32 +1,77 @@
 return {
     "stevearc/conform.nvim",
-    config = function()
-        require("conform").setup({
-            notify_on_error = false,
-            -- auto formatting
-            format_on_save = {
-                timeout_ms = 500,
-                lsp_fallback = true
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+        {
+            -- Customize or remove this keymap to your liking
+            "<leader>f",
+            function()
+                require("conform").format({ async = true, lsp_fallback = true })
+            end,
+            mode = "",
+            desc = "Format buffer",
+        },
+    },
+    -- Everything in opts will be passed to setup()
+    opts = {
+        -- Define your formatters
+        formatters_by_ft = {
+            lua = { "stylua" },
+            -- Conform will run multiple formatters sequentially
+            -- python = { "black" },
+            python = { "isort", "black" },
+            -- Use a sub-list to run only the first available formatter
+            javascript = { { "prettier" } },
+            javascriptreact = { { "prettier" } },
+            typescript = { { "prettier" } },
+            typescriptreact = { { "prettier" } },
+            go = { "goimports", "gofmt" },
+        },
+        -- Set up format-on-save
+        format_on_save = { timeout_ms = 500, lsp_fallback = true },
+        -- Customize formatters
+        formatters = {
+            shfmt = {
+                prepend_args = { "-i", "2" },
             },
-            formatters_by_ft = {
-                lua = {"stylua"},
-                -- Conform will run multiple formatters sequentially
-                python = {"isort", "black"},
-                -- Use a sub-list to run only the first available formatter
-                javascript = {{"biomejs", "prettier"}},
-                javascriptreact = {{"biomejs", "prettier"}},
-                typescript = {{"biomejs", "prettier"}},
-                typescriptreact = {{"biomejs", "prettier"}},
-                go = {"goimports", "gofmt"}
-            }
-        })
-        vim.keymap.set({"n", "v"}, "<leader>f", function()
-            require("conform").format({
-                lsp_fallback = true,
-                timeout_ms = 500
-            })
-        end, {
-            desc = "Format file or range using Conform"
-        })
-    end
+        },
+    },
+    -- init = function()
+    --     -- If you want the formatexpr, here is the place to set it
+    --     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    -- end,
 }
+-- return {
+--     "stevearc/conform.nvim",
+--     config = function()
+--         require("conform").setup({
+--             notify_on_error = false,
+--             -- auto formatting
+--             format_on_save = {
+--                 timeout_ms = 500,
+--                 lsp_fallback = true,
+--             },
+--             formatters_by_ft = {
+--                 lua = { "stylua" },
+--                 -- Conform will run multiple formatters sequentially
+--                 -- python = { "black" },
+--                 python = { "isort", "black" },
+--                 -- Use a sub-list to run only the first available formatter
+--                 javascript = { { "prettier" } },
+--                 javascriptreact = { { "prettier" } },
+--                 typescript = { { "prettier" } },
+--                 typescriptreact = { { "prettier" } },
+--                 go = { "goimports", "gofmt" },
+--             },
+--         })
+--         vim.keymap.set({ "n", "v" }, "<leader>f", function()
+--             require("conform").format({
+--                 lsp_fallback = true,
+--                 timeout_ms = 500,
+--             })
+--         end, {
+--             desc = "Format file or range using Conform",
+--         })
+--     end,
+-- }
