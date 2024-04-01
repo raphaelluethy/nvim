88f791799e39dba476b31b353642c01bdbfd640e
Raphael Lüthy <raphael.luethy@fhnw.ch>
2024-04-01 18:55:48 +0200

    feat: finished

--- bak/lua/raphaelluethy/plugins/blankline.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-        require("ibl").setup({
-            debounce = 100,
-            whitespace = { highlight = { "Whitespace" } },
-        })
-    end,
-}
