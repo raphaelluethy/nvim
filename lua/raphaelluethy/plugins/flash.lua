7502b6aaf6238765605def76796cba0fd5d2150c
Raphael Lüthy <raphael.luethy@fhnw.ch>
2024-04-16 12:03:52 +0200

    feat: jump

---
+++ lua/raphaelluethy/plugins/flash.lua
@@ -0,0 +1,37 @@
+return {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    -- stylua: ignore
+    keys = {
+        { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+        { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+        { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+        { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    },
+    config = function()
+        require('flash').setup({
+            modes = {
+                char = {
+                    jump_labels = true,
+                },
+            },
+            highlight = {
+                -- show a backdrop with hl FlashBackdrop
+                backdrop = true,
+                -- Highlight the search matches
+                matches = true,
+                -- extmark priority
+                priority = 5000,
+                groups = {
+                    match = "FlashMatch",
+                    current = "FlashCurrent",
+                    backdrop = "FlashBackdrop",
+                    label = "FlashLabel",
+                },
+            },
+        })
+        vim.cmd.hi 'FlashLabel guifg=#FCE566'
+    end
+}
