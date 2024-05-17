e22c880469677ea933fd40d143149bed689a7904
Raphael Lüthy <raphael.luethy@fhnw.ch>
2024-05-17 15:50:43 +0200

    feat: clean up and added dap

---
+++ lua/raphaelluethy/plugins/dap.lua
@@ -0,0 +1,129 @@
+return {
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "leoluz/nvim-dap-go",
+            "rcarriga/nvim-dap-ui",
+            "theHamsta/nvim-dap-virtual-text",
+            "nvim-neotest/nvim-nio",
+            "williamboman/mason.nvim",
+        },
+        config = function()
+            local dap = require "dap"
+            local ui = require "dapui"
+
+            require("dapui").setup()
+            require("dap-go").setup()
+
+            require("nvim-dap-virtual-text").setup {
+                -- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
+                display_callback = function(variable)
+                    local name = string.lower(variable.name)
+                    local value = string.lower(variable.value)
+
+                    if #variable.value > 15 then
+                        return " " .. string.sub(variable.value, 1, 15) .. "... "
+                    end
+
+                    return " " .. variable.value
+                end,
+            }
+
+            -- Go
+
+            dap.adapters.delve = {
+                type = 'server',
+                port = '${port}',
+                executable = {
+                    command = 'dlv',
+                    args = { 'dap', '-l', '127.0.0.1:${port}' },
+                    -- add this if on windows, otherwise server won't open successfully
+                    -- detached = false
+                }
+            }
+
+            -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+            dap.configurations.go = {
+                {
+                    type = "delve",
+                    name = "Debug",
+                    request = "launch",
+                    program = "${file}"
+                },
+                {
+                    type = "delve",
+                    name = "Debug test", -- configuration for debugging test files
+                    request = "launch",
+                    mode = "test",
+                    program = "${file}"
+                },
+                -- works with go.mod packages and sub packages
+                {
+                    type = "delve",
+                    name = "Debug test (go.mod)",
+                    request = "launch",
+                    mode = "test",
+                    program = "./${relativeFileDirname}"
+                }
+            }
+
+            -- Handled by nvim-dap-go
+            -- dap.adapters.go = {
+            --   type = "server",
+            --   port = "${port}",
+            --   executable = {
+            --     command = "dlv",
+            --     args = { "dap", "-l", "127.0.0.1:${port}" },
+            --   },
+            -- }
+
+            -- local elixir_ls_debugger = vim.fn.exepath "elixir-ls-debugger"
+            -- if elixir_ls_debugger ~= "" then
+            --     dap.adapters.mix_task = {
+            --         type = "executable",
+            --         command = elixir_ls_debugger,
+            --     }
+
+            --     dap.configurations.elixir = {
+            --         {
+            --             type = "mix_task",
+            --             name = "phoenix server",
+            --             task = "phx.server",
+            --             request = "launch",
+            --             projectDir = "${workspaceFolder}",
+            --             exitAfterTaskReturns = false,
+            --             debugAutoInterpretAllModules = false,
+            --         },
+            --     }
+            -- end
+
+            vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+            vim.keymap.set("n", "<space>rb", dap.run_to_cursor)
+
+            -- Eval var under cursor
+            vim.keymap.set("n", "<space>?", function()
+                require("dapui").eval(nil, { enter = true })
+            end)
+
+            vim.keymap.set("n", "<F1>", dap.continue)
+            vim.keymap.set("n", "<F2>", dap.step_into)
+            vim.keymap.set("n", "<F3>", dap.step_over)
+            vim.keymap.set("n", "<F4>", dap.step_out)
+            vim.keymap.set("n", "<F5>", dap.step_back)
+            vim.keymap.set("n", "<F13>", dap.restart)
+
+            dap.listeners.before.attach.dapui_config = function()
+                ui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                ui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                ui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                ui.close()
+            end
+        end,
+    },
+}
