2b34f7d6d8526a9fc18915459dea917aa1ef8838
Raphael Lüthy <raphael.luethy@fhnw.ch>
2024-06-06 09:57:34 +0200

    feat: updated config

--- lua/raphaelluethy/plugins/trouble.lua
+++ lua/raphaelluethy/plugins/trouble.lua
@@ -1,37 +1,36 @@
 return {
-  'folke/trouble.nvim',
-  branch = 'dev', -- IMPORTANT!
-  keys = {
-    {
-      '<leader>tt',
-      '<cmd>Trouble diagnostics toggle<cr>',
-      desc = 'Diagnostics (Trouble)',
+    'folke/trouble.nvim',
+    keys = {
+        {
+            '<leader>tt',
+            '<cmd>Trouble diagnostics toggle<cr>',
+            desc = 'Diagnostics (Trouble)',
+        },
+        {
+            '<leader>tT',
+            '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+            desc = 'Buffer Diagnostics (Trouble)',
+        },
+        {
+            '<leader>cs',
+            '<cmd>Trouble symbols toggle focus=false<cr>',
+            desc = 'Symbols (Trouble)',
+        },
+        {
+            '<leader>cl',
+            '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+            desc = 'LSP Definitions / references / ... (Trouble)',
+        },
+        {
+            '<leader>tL',
+            '<cmd>Trouble loclist toggle<cr>',
+            desc = 'Location List (Trouble)',
+        },
+        {
+            '<leader>tQ',
+            '<cmd>Trouble qflist toggle<cr>',
+            desc = 'Quickfix List (Trouble)',
+        },
     },
-    {
-      '<leader>tT',
-      '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
-      desc = 'Buffer Diagnostics (Trouble)',
-    },
-    {
-      '<leader>cs',
-      '<cmd>Trouble symbols toggle focus=false<cr>',
-      desc = 'Symbols (Trouble)',
-    },
-    {
-      '<leader>cl',
-      '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-      desc = 'LSP Definitions / references / ... (Trouble)',
-    },
-    {
-      '<leader>tL',
-      '<cmd>Trouble loclist toggle<cr>',
-      desc = 'Location List (Trouble)',
-    },
-    {
-      '<leader>tQ',
-      '<cmd>Trouble qflist toggle<cr>',
-      desc = 'Quickfix List (Trouble)',
-    },
-  },
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
 }
