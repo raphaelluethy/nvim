c3422c97a8d57c0a7e4bce7f3395c4eb121bdb42
Raphael Lüthy <raphael.luethy@fhnw.ch>
2024-08-03 22:14:44 -0700

    feat: upsated packages
    Signed-off-by: Raphael Lüthy <raphael.luethy@fhnw.ch>

--- lua/raphaelluethy/plugins/which_key.lua
+++ lua/raphaelluethy/plugins/which_key.lua
@@ -1,16 +1,25 @@
-return {                -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-        require('which-key').setup()
+return {
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
 
-        -- Document existing key chains
-        require('which-key').register {
-            ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-            ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-            ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-            ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-            ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        }
-    end,
+    { '<leader>c', group = '[C]ode' },
+    { '<leader>c_', hidden = true },
+    { '<leader>d', group = '[D]ocument' },
+    { '<leader>d_', hidden = true },
+    { '<leader>r', group = '[R]ename' },
+    { '<leader>r_', hidden = true },
+    { '<leader>s', group = '[S]earch' },
+    { '<leader>s_', hidden = true },
+    { '<leader>w', group = '[W]orkspace' },
+    { '<leader>w_', hidden = true },
+  },
 }
