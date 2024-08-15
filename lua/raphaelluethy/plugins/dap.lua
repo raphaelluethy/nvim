e84abfc0e3824379f1ae1e0e1ffeed8cc023bbd3
Raphael Lüthy <raphael.luethy@fhnw.ch>
2024-08-15 13:54:22 +0200

    feat: js debug works now!

--- lua/raphaelluethy/plugins/dap.lua
+++ lua/raphaelluethy/plugins/dap.lua
@@ -8,7 +8,8 @@ return {
             "theHamsta/nvim-dap-virtual-text",
             "nvim-neotest/nvim-nio",
             "williamboman/mason.nvim",
-            "jay-babu/mason-nvim-dap.nvim"
+            "jay-babu/mason-nvim-dap.nvim",
+            "mxsdev/nvim-dap-vscode-js"
         },
         config = function()
             local dap = require "dap"
@@ -31,9 +32,55 @@ return {
                     'python',
                     'javadbg',
                     'javatest',
-                    'kotlin'
+                    'kotlin',
+                    'js'
                 },
             }
+            require('dap-vscode-js').setup({
+                debugger_path = '~/.local/share/lvim/mason/packages/js-debug-adapter',
+                debugger_cmd = { 'js-debug-adapter' },
+                adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' },
+                node_path = 'node'
+            })
+
+            -- if I get the sourcemap error again, just do this: https://github.com/mxsdev/nvim-dap-vscode-js/issues/35#issuecomment-1458311404
+
+            for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
+                dap.configurations[language] = {
+                    {
+                        type = "pwa-node",
+                        request = "launch",
+                        name = "Debug TypeScript",
+                        program = "${workspaceFolder}/path/to/your/ts-file.ts",
+                        runtimeExecutable = "node",
+                        runtimeArgs = { "-r", "ts-node/register" },
+                        outFiles = { "${workspaceFolder}/path/to/your/ts-file.js" },
+                        sourceMaps = true,
+                        stopOnEntry = false,
+                        args = {},
+                        cwd = "${workspaceFolder}",
+                        protocol = "inspector",
+                        console = "integratedTerminal",
+                        internalConsoleOptions = "openOnSessionStart"
+                    },
+                    {
+                        type = "pwa-node",
+                        request = "launch",
+                        name = "Launch file",
+                        program = "${file}",
+                        cwd = "${workspaceFolder}",
+                        sourceMaps = true,
+                        skipFiles = { "<node_internals>/**", "node_modules/**" },
+                    },
+                    {
+                        type = "pwa-node",
+                        request = "attach",
+                        name = "Attach",
+                        processId = require 'dap.utils'.pick_process,
+                        cwd = "${workspaceFolder}",
+                    } }
+            end
+
 
             -- Dap UI setup
             -- For more information, see |:help nvim-dap-ui|
@@ -42,8 +89,6 @@ return {
             require("dap-go").setup()
 
             require("nvim-dap-virtual-text").setup {}
-
-
 
             -- -- Handled by nvim-dap-go
             -- dap.adapters.go = {
