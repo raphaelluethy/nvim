aa140fa4c07517d75b68b326c3768db10832a5dd
Raphael Lüthy <raphael.luethy@fhnw.ch>
2024-08-20 09:36:08 +0200

    feat: refactor llm functions for groq, anthropic, and ollama with help commands and distinguish replace logic

--- init.lua
+++ init.lua
@@ -3,52 +3,58 @@ require 'raphaelluethy.config.auto_cmds'
 require 'raphaelluethy.config.commands'
 require 'raphaelluethy.config.keymaps'
 
-
-
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins
-
-
-require('lazy').setup({ { import = "raphaelluethy.plugins" } }, {
-    ui = {
-        -- If you are using a Nerd Font: set icons to an empty table which will use the
-        -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-        icons = vim.g.have_nerd_font and {} or {
-            cmd = '⌘',
-            config = '🛠',
-            event = '📅',
-            ft = '📂',
-            init = '⚙',
-            keys = '🗝',
-            plugin = '🔌',
-            runtime = '💻',
-            require = '🌙',
-            source = '📄',
-            start = '🚀',
-            task = '📌',
-            lazy = '💤 ',
-        },
-    },
-    silent = true,
-})
+-- if I need to use vscode, at least its still neovim
+if not vim.g.vscode then
+    setup()
+end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+function setup()
+    -- [[ Install `lazy.nvim` plugin manager ]]
+    --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+    local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+    if not vim.loop.fs_stat(lazypath) then
+        local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+        vim.fn.system {'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath}
+    end ---@diagnostic disable-next-line: undefined-field
+    vim.opt.rtp:prepend(lazypath)
+
+    -- [[ Configure and install plugins ]]
+    --
+    --  To check the current status of your plugins, run
+    --    :Lazy
+    --
+    --  You can press `?` in this menu for help. Use `:q` to close the window
+    --
+    --  To update plugins you can run
+    --    :Lazy update
+    --
+    -- NOTE: Here is where you install your plugins
+
+    require('lazy').setup({{
+        import = "raphaelluethy.plugins"
+    }}, {
+        ui = {
+            -- If you are using a Nerd Font: set icons to an empty table which will use the
+            -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+            icons = vim.g.have_nerd_font and {} or {
+                cmd = '⌘',
+                config = '🛠',
+                event = '📅',
+                ft = '📂',
+                init = '⚙',
+                keys = '🗝',
+                plugin = '🔌',
+                runtime = '💻',
+                require = '🌙',
+                source = '📄',
+                start = '🚀',
+                task = '📌',
+                lazy = '💤 '
+            }
+        },
+        silent = true
+    })
+end
--- lua/raphaelluethy/plugins/custom_llm.lua
+++ lua/raphaelluethy/plugins/custom_llm.lua
@@ -6,7 +6,7 @@ return {
       'You are an AI programming assistant. Follow the instructions carefully. Do omit any explanations. Do not talk at all. Only output valid code. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks'
     local llm = require 'llm'
 
-    local function groq()
+    local function groq_replace()
       llm.invoke_llm_and_stream_into_editor({
         url = 'https://api.groq.com/openai/v1/chat/completions',
         model = 'llama-3.1-70b-versatile',
@@ -15,8 +15,17 @@ return {
         replace = true,
       }, llm.make_openai_spec_curl_args, llm.handle_openai_spec_data)
     end
+    local function groq_help()
+      llm.invoke_llm_and_stream_into_editor({
+        url = 'https://api.groq.com/openai/v1/chat/completions',
+        model = 'llama-3.1-70b-versatile',
+        api_key_name = 'GROQ_API_KEY',
+        system_prompt = coding_promt,
+        replace = false,
+      }, llm.make_openai_spec_curl_args, llm.handle_openai_spec_data)
+    end
 
-    local function anthropic()
+    local function anthropic_replace()
       llm.invoke_llm_and_stream_into_editor({
         url = 'https://api.anthropic.com/v1/messages',
         model = 'claude-3-5-sonnet-20240620',
@@ -25,8 +34,17 @@ return {
         replace = true,
       }, llm.make_anthropic_spec_curl_args, llm.handle_anthropic_spec_data)
     end
+    local function anthropic_help()
+      llm.invoke_llm_and_stream_into_editor({
+        url = 'https://api.anthropic.com/v1/messages',
+        model = 'claude-3-5-sonnet-20240620',
+        api_key_name = 'ANTHROPIC_API_KEY',
+        system_prompt = coding_promt,
+        replace = false,
+      }, llm.make_anthropic_spec_curl_args, llm.handle_anthropic_spec_data)
+    end
 
-    local function ollama()
+    local function ollama_replace()
       llm.invoke_llm_and_stream_into_editor({
         url = 'http://localhost:11434/api/chat',
         model = 'mistral-nemo',
@@ -34,15 +52,32 @@ return {
         replace = true,
       }, llm.make_ollama_spec_curl_args, llm.handle_ollama_spec_data)
     end
+    local function ollama_help()
+      llm.invoke_llm_and_stream_into_editor({
+        url = 'http://localhost:11434/api/chat',
+        model = 'mistral-nemo',
+        system_prompt = coding_promt,
+        replace = false,
+      }, llm.make_ollama_spec_curl_args, llm.handle_ollama_spec_data)
+    end
 
-    vim.keymap.set({ 'n', 'v' }, '<leader>llg', groq, {
+    vim.keymap.set({ 'n', 'v' }, '<leader>lgr', groq_replace, {
       desc = 'llm groq',
     })
-    vim.keymap.set({ 'n', 'v' }, '<leader>lla', anthropic, {
+    vim.keymap.set({ 'n', 'v' }, '<leader>lgh', groq_help, {
+      desc = 'llm groq help',
+    })
+    vim.keymap.set({ 'n', 'v' }, '<leader>lar', anthropic_replace, {
       desc = 'llm anthropic_help',
     })
-    vim.keymap.set({ 'n', 'v' }, '<leader>llo', ollama, {
+    vim.keymap.set({ 'n', 'v' }, '<leader>lah', anthropic_help, {
+      desc = 'llm anthropic help',
+    })
+    vim.keymap.set({ 'n', 'v' }, '<leader>lor', ollama_replace, {
       desc = 'llm ollama',
+    })
+    vim.keymap.set({ 'n', 'v' }, '<leader>loh', ollama_help, {
+      desc = 'llm ollama help',
     })
   end,
 }
