c6a1ed5e32da824a43553c86470c2ecd03c0471a
Raphael Lüthy <raphael@lty.gg>
2025-08-19 22:44:03 +0200

    chore: disable plugins and update lockfile

--- lua/raphaelluethy/plugins/ninetiyfive.lua
+++ lua/raphaelluethy/plugins/ninetiyfive.lua
@@ -1,12 +1,12 @@
 return {
-  'https://github.com/ninetyfive-gg/ninetyfive.nvim',
-  config = function()
-    require('ninetyfive').setup {
-      indexing = {
-        mode = 'on', -- enables code indexing for better completions. 'ask' by default.
-        cache_consent = false, -- optional, defaults to true
-      },
-    }
-  end,
-  version = false, -- we don't have versioning in the plugin yet
+  -- 'https://github.com/ninetyfive-gg/ninetyfive.nvim',
+  -- config = function()
+  --   require('ninetyfive').setup {
+  --     indexing = {
+  --       mode = 'on', -- enables code indexing for better completions. 'ask' by default.
+  --       cache_consent = false, -- optional, defaults to true
+  --     },
+  --   }
+  -- end,
+  -- version = false, -- we don't have versioning in the plugin yet
 }
--- lua/raphaelluethy/plugins/oil.lua
+++ lua/raphaelluethy/plugins/oil.lua
@@ -1,20 +1,20 @@
 return {
-  {
-    'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
-    -- Optional dependencies
-    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-    config = function()
-      require('oil').setup {
-        view_options = {
-          -- Show files and directories that start with "."
-          show_hidden = true,
-        },
-      }
-      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open Oil.nvim' })
-    end,
-  },
+  -- {
+  --   'stevearc/oil.nvim',
+  --   ---@module 'oil'
+  --   ---@type oil.SetupOpts
+  --   opts = {},
+  --   -- Optional dependencies
+  --   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  --   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  --   config = function()
+  --     require('oil').setup {
+  --       view_options = {
+  --         -- Show files and directories that start with "."
+  --         show_hidden = true,
+  --       },
+  --     }
+  --     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open Oil.nvim' })
+  --   end,
+  -- },
 }
