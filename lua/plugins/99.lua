82d9544f38ac5330a9875558745efaec3665316f
Raphael Lüthy <raphael@lty.gg>
2026-01-29 19:53:39 +0100

    refactor(99): remove commented out debug code and logic

--- lua/plugins/99.lua
+++ lua/plugins/99.lua
@@ -4,16 +4,8 @@ return {
 	config = function()
 		local _99 = require("99")
 
-		-- local cwd = vim.uv.cwd()
-		-- local basename = vim.fs.basename(cwd)
-
 		_99.setup({
 			model = "opencode/kimi-k2.5",
-			-- logger = {
-			-- 	level = _99.DEBUG,
-			-- 	path = "/tmp/" .. basename .. ".99.debug",
-			-- 	print_on_error = true,
-			-- },
 			completion = {
 				source = "cmp",
 			},
